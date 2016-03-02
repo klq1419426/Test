3c3988f572f34510006d150d01d99f70906873e0
klq1419426 <1419426440@qq.com>
2016-03-02 21:49:40 +0800

    the 4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("1111111111111111111");
-		System.out.println("3333333333333333333");
+		System.out.println("6666666666666666666");
 	}
 }

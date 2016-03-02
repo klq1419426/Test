40920a6f45e7b920555dcbeb696fdfed2497588b
klq1419426 <1419426440@qq.com>
2016-03-02 21:32:13 +0800

    the3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("1111111111111111111");
-		System.out.println("5555555555555555555");
+		System.out.println("3333333333333333333");
 	}
 }
